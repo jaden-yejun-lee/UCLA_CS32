8f126fb48a0ad097be67dff09edd2e1ddba78758
Eel-Nedaj <96278299+Eel-Nedaj@users.noreply.github.com>
2022-03-06 15:11:50 -0800

    Create MemberDatabase.h

---
+++ Project 4/MemberDatabase.h
@@ -0,0 +1,28 @@
+#ifndef _MEMBERDATABASE_H
+#define _MEMBERDATABASE_H
+
+#include "provided.h"
+#include "PersonProfile.h"
+#include "RadixTree.h"
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class MemberDatabase {
+public:
+	MemberDatabase();
+	~MemberDatabase();
+
+	bool LoadDatabase(string filename);
+	vector<string> FindMatchingMembers(const AttValPair& input) const;
+	const PersonProfile* GetMemberByEmail(string email) const;
+
+
+private:
+	RadixTree<PersonProfile> m_emails;
+	RadixTree<vector<string>> memberVector;
+};
+
+#endif
+ 
