b86a0c2309ad7fee3d9993544a60f2717cadf001
Eel-Nedaj <96278299+Eel-Nedaj@users.noreply.github.com>
2022-03-28 19:54:58 -0700

    Update MatchMaker.cpp

--- Project 4/MatchMaker.cpp
+++ Project 4/MatchMaker.cpp
@@ -1,14 +1,15 @@
 #include "MatchMaker.h"
 #include "provided.h"
 #include <vector>
+#include <unordered_set>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 MatchMaker::MatchMaker(const MemberDatabase& mdb, const AttributeTranslator& at) {
-	m_mdb = mdb;
-	m_at = at;
+	m_mdb = new MemberDatabase(mdb);
+	m_at = new AttributeTranslator(at);
 }
 
 MatchMaker::~MatchMaker() {
@@ -19,7 +20,8 @@ MatchMaker::~MatchMaker() {
 vector<EmailCount> MatchMaker::IdentifyRankedMatches(string email, int threshold) const {
 
 	// retrieve person's PersonProfile from email 
-	const PersonProfile* p = m_mdb.GetMemberByEmail(email);
+	const PersonProfile* p = m_mdb->GetMemberByEmail(email);
+
 
 	// obtain person's attribute-value pairs
 	// push unique compatible attribute-value pairs into pairVectors
@@ -28,7 +30,7 @@ vector<EmailCount> MatchMaker::IdentifyRankedMatches(string email, int threshold
 		for (int i = 0; i < p->GetNumAttValPairs(); i++) {
 			AttValPair pair;
 			p->GetAttVal(i, pair);
-			vector<AttValPair>tempVector = m_at.FindCompatibleAttValPairs(pair);
+			vector<AttValPair>tempVector = m_at->FindCompatibleAttValPairs(pair);
 			for (int j = 0; j != tempVector.size(); j++) {
 				auto itr = find(pairVectors.begin(), pairVectors.end(), tempVector[j]);
 				if (itr == pairVectors.end()) {
@@ -38,37 +40,35 @@ vector<EmailCount> MatchMaker::IdentifyRankedMatches(string email, int threshold
 		}
 	}
 
-	// NEED TO REWRITE THIS PART CUZ IT TAKES TOO LONG 
-	// ITERATOR AND FOR LOOP NOT GOOD
-
 	// iterate through compatible attributes and match attribute with
 	// collection of members that have each compatible attribute-value pair
+
 	auto it = pairVectors.begin();
+	
+	map<string, int> m_map;
 	vector<EmailCount> emailCollection;
+
 	while (it != pairVectors.end()) {
 
 		AttValPair find(it->attribute, it->value);
-		vector<string> matchingEmails = m_mdb.FindMatchingMembers(find);
+		vector<string> matchingEmails = m_mdb->FindMatchingMembers(find);
 
 		for (int i = 0; i != matchingEmails.size(); i++) {
 
-			if (matchingEmails[i] == email) {
+			string s = matchingEmails[i];
+
+			if (s == email) {
 				continue;
 			}
 
-			bool duplicate = false;
-			for (int j = 0; j != emailCollection.size(); j++) {
-				if (matchingEmails[i] == emailCollection[j].email) {
-					duplicate = true;
-					emailCollection[j].count++;
-				}
-			}
+			auto found = m_map.find(s);
+			if (found == m_map.end()) {
+				m_map.insert(pair<string, int>(s, 1));
 
-			if (duplicate == false) {
-				EmailCount tempEC(matchingEmails[i], 1);
-				emailCollection.push_back(tempEC);
 			}
-
+			else {
+				found->second++;
+			}
 		}
 
 		it++;
@@ -77,24 +77,24 @@ vector<EmailCount> MatchMaker::IdentifyRankedMatches(string email, int threshold
 
 	// create a collection of members that have at least the 
 	// threshold number of compatible attribute-value pairs
+	for (auto it = m_map.begin(); it != m_map.end(); it++) {
 
-	// sort in descending order with ties broken by ascending alphabetical order
-	 sort(emailCollection.begin(), emailCollection.end(), [](EmailCount x, EmailCount y) {
-			 if (x.count == y.count) {
-				 return x.email < y.email;
-			 }
-			 else {
-				 return x.count > y.count;
-			 }
-		 });
+		EmailCount tempEC(it->first, it->second);
+		emailCollection.push_back(tempEC);
 
-	for (int i = 0; i != emailCollection.size(); i++) {
-		// cerr << emailCollection[i].email << endl;
 	}
 
+	// sort in descending order with ties broken by ascending alphabetical order
+	sort(emailCollection.begin(), emailCollection.end(), [](EmailCount x, EmailCount y) {
+		if (x.count == y.count) {
+			return x.email < y.email;
+		}
+		else {
+			return x.count > y.count;
+		}
+		});
 
 	vector<EmailCount> v;
-
 	for (int i = 0; i != emailCollection.size(); i++) {
 		if (emailCollection[i].count >= threshold) {
 			//insert
@@ -103,7 +103,6 @@ vector<EmailCount> MatchMaker::IdentifyRankedMatches(string email, int threshold
 	}
 
 	return v;
-
-
 }
+
 
